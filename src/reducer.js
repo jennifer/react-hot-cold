fc33b06229e6d6ebaf5407a419e9908ad10bf0d8
jennifer kerssen <jjkerssen@gmail.com>
2018-07-17 19:16:18 -0600

    add provider tag in index

--- src/reducer.js
+++ src/reducer.js
@@ -53,6 +53,8 @@ export default (state = initialState, action) => {
     if (action.type === GENERATE_AURAL_UPDATE) {
         const {guesses, feedback} = state;
 
+        // If there's not exactly 1 guess, we want to
+        // pluralize the nouns in this aural update.
         const pluralize = guesses.length !== 1;
 
         let auralStatus = `Here's the status of the game right now: ${feedback} You've made ${guesses.length} ${pluralize
